bd676dcaa67b2ca91f1c2b541565f92308c3f1b2
Masashi Iizuka <liquidz.uo@gmail.com>
2012-04-01 03:02:20 +0900

    update

--- project/update.sh
+++ project/update.sh
@@ -4,6 +4,6 @@
 rm -rf misaki/*
 cp -pir $HOME/lisp/misaki/sample/public/* misaki/
 
-find misaki -type f -name "*.html" -exec perl -p -i -e 's/"\//"\/project\/misaki\//g' {} \;
+find misaki -type f -name "*.html" -exec perl -p -i -e 's/"\/(?!"\))/"\/project\/misaki\//g' {} \;
 find misaki -type f -name "main.css" -exec perl -p -i -e 's/\/img/\/project\/misaki\/img/g' {} \;
 
