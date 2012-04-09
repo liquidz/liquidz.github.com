ed682e51b6aef623ae88bb0de952f1183b52f6e8
Masashi Iizuka <liquidz.uo@gmail.com>
2012-04-10 00:30:26 +0900

    ポスト追加

---
+++ _template/_posts/2012-04-10-misaki-blog.html.clj
@@ -0,0 +1,16 @@
+; @layout post
+; @title  ブログをJekyllからmisakiに移行したった
+
+(p "ちょこちょこ直しながらの作業だったので時間がかかってしまいましたが、
+   ブログを" (html/link "misaki" "https://github.com/liquidz/misaki") "に移行しました。")
+
+
+(p "まだ改善すべき点は多くあって、HTMLに比べれば楽に書けるけど
+   Markdownには劣ってる(それでも個人的にはMarkdownの記法がうるおぼえなのでmisakiの方が迷わずに書けますが)
+   レベルなので、これからもっと自分が使いやすいよう修正していけたらと思っています。")
+
+(p "まだまだ汚いブログのコードは以下。テンプレート内関数など多用してます。")
+
+(p (html/link "https://github.com/liquidz/liquidz.github.com/tree/master/_template"))
+
+(p "ひとまずご報告まで")
